3895e5c267e7f0694e7ab2e486ba06ae58e3a37b
Artemiy <142483701+memasik03@users.noreply.github.com>
2024-03-15 12:12:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const searchInputRef = useRef(null);
 
-const hideKeyboardOnScroll = () => {
-    const handleScroll = () => {
-        if (searchInputRef.current) {
-            searchInputRef.current.blur();
-        }
-        document.removeEventListener("scroll", handleScroll, true);
-    };
-
-    document.addEventListener("scroll", handleScroll, true);
-};
